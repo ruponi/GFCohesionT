90d9feadbf2f3e0b8ae671c1df0f2243890d7db1
Ruslan Ponomarenko <ruponi@gmail.com>
2021-12-31 00:02:38 +0200

    Protocol for DataLayer

---
+++ GFCohesionT/DataLayerPT.swift
@@ -0,0 +1,17 @@
+//
+//  DataLayerPT.swift
+//  GFCohesionT
+//
+//  Created by Ruslan Ponomarenko on 12/31/21.
+//
+
+import Foundation
+
+/// Common protocol for all types of the datalayers
+protocol DataLayerPT{
+    func submitStatus(userID: String, status: GFuserStatus, regionID: String, result: @escaping (Bool) -> ())
+    //mockup  functions
+    func getUser(with userID: String, result: @escaping (User?)->() )
+    func getOffice(with officeID: String,result: @escaping (Office?)->() )
+    
+}
